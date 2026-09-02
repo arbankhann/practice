654f9982723651a2a156b1c95db8349b99bd137b
Arban <ak@alphainfolab.com>
2026-09-02 12:08:21 +0530

    np

--- text.py
+++ text.py
@@ -1,6 +1,45 @@
-print("hello")
-print("arban  khan")
-print("insha khan")
-print("hello neha")
-print("hello roshni")
-print("hello daraksha")
+# print("hello")
+# print("arban  khan")
+# print("insha khan")
+# print("hello neha")
+# print("hello roshni")
+# print("hello daraksha")
+import numpy as np
+# A=np.array([90,21,78])
+# B=np.array([21,23,45])
+# print(A*B)
+# C=np.array([
+#     [1,3],
+#     [4,5]
+# ])
+# # [
+# [4,5],
+# [5,6]
+# ]])
+# print(C.ndim)
+# print(C.shape)
+# d=np.linalg.det(C)
+# print(d)
+
+# N=np.array([
+#     [1,2],
+#     [3,4]
+# ])
+# M=np.array([
+#     [3,4],
+#     [5,6]
+# ])
+# O= N@M
+# print(O)
+# print(N)
+# # print(M)
+# evalue,evactor=np.linalg.eig(N)
+# print("evalue:",evalue)
+# print("evactor:",evactor)
+# t=np.kron(M,N)
+# print(t)
+ 
+C=np.array([
+    3+4j
+])
+print(C.imag+C.real)
