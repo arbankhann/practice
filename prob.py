51766c0290b50e4bf1520cf31aac16b5d76820e0
Arban <ak@alphainfolab.com>
2026-09-08 11:15:14 +0530

    probability

--- prob.py
+++ prob.py
@@ -24,13 +24,13 @@
 
 # even = np.sum(dice % 2 == 0)
 
-# probability = even / 2
+probability = even / 2
 
-# print(dice)
-# print(even)
-# print(probability)
-# dice=np.random.choice([1,2,3,4,5,6])
-# print(dice)
+print(dice)
+print(even)
+print(probability)
+dice=np.random.choice([1,2,3,4,5,6])
+print(dice)
 
 
 import numpy as np 
